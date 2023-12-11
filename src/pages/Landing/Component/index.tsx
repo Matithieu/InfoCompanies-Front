dff98c497ac8204633d8ab2e7298ea3907a3ce4e
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2023-12-11 21:19:01 +0100

    feat: add buttons to redirect

--- src/pages/Landing/Component/index.tsx
+++ src/pages/Landing/Component/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
+    const navigate = useNavigate();
     return (
         <div>
             <h1>Landing</h1>
-        </div>
+            <div style={{margin: "10px"}}>
+                <button style={{marginRight: "10px"}} onClick={() => navigate("/login")}>Go to Login</button>
+                <button onClick={() => navigate("/dashboard")}>Go to Dashboard</button>
+            </div>
+        </div >
     );
 }
