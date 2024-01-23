e7d96a5a0a9ca4759af2561b98bf64a55e4e9526
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-01-23 21:27:12 +0100

    fix: refactor Favorites page

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -12,18 +12,23 @@ import { TableSkeleton } from '../../components/Skeleton/index.tsx';
 
 export default function Favorites() {
     const [url, setUrl] = useState<string>("");
+    const [checkedCompanies, setCheckedCompanies] = useState<Array<String>>([]);
 
     useEffect(() => {
         const idsOfCheckedCompanies: Array<String> = JSON.parse(localStorage.getItem('checkedToDo') || '[]');
+        setCheckedCompanies(idsOfCheckedCompanies);
         //TODO: fetch the list of companies from the api using the sirensOfCheckedCompanies and then put it in setListOfCompanies
         console.log("idsOfCheckedCompanies ", idsOfCheckedCompanies);
+        console.log("checkedCompanies ", checkedCompanies);
 
         setUrl(`api/v1/companies-by-ids?ids=${idsOfCheckedCompanies}&`);
         console.log("url: ", `api/v1/companies-by-ids?ids=${idsOfCheckedCompanies}&`);
     }, []);
 
+    // ...
+
     return (
-        <Grid >
+        <Grid>
             <CssBaseline />
 
             <Typography fontFamily={"Poppins"} variant="h4" component="div" align="left" marginTop={5} marginLeft={10} marginBottom={5}>
@@ -42,101 +47,87 @@ export default function Favorites() {
                                 sx={{
                                     display: 'flex',
                                     flexDirection: 'column',
-                                    borderRadius: 3
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    borderRadius: 3,
+                                    minHeight: 400,
+                                    maxHeight: 400
                                 }}
                             >
-                                {url ? <TableCompany url={url} /> : <TableSkeleton columns={columns} />}
+                                {checkedCompanies.length !== 0 ? (
+                                    url ? <TableCompany url={url} /> : <TableSkeleton columns={columns} />
+                                ) : (
+                                    <a style={{ fontSize: '19px' }}>Aucun ToDO sélectionné</a>
+                                )}
                             </Paper>
                         </Grid>
 
                         {/* Details of the company */}
-                        <Grid item xs={12} md={4}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    minHeight: 200,
-                                    borderRadius: 3
-                                }}
-                            >
-                                <Details />
-                            </Paper>
-                        </Grid>
+                        {checkedCompanies.length !== 0 && (
+                            <Grid item xs={12} md={4}>
+                                <Paper
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        minHeight: 200,
+                                        borderRadius: 3
+                                    }}
+                                >
+                                    <Details />
+                                </Paper>
+                            </Grid>
+                        )}
                     </Grid>
                 </Grid>
 
                 {/* Container des éléments sur la deuxième ligne */}
-                <Grid item xs={12} md={12} lg={12}>
+                {checkedCompanies.length !== 0 && (
+                    <>
+                        <Grid item xs={12} md={12} lg={12}>
+                            <Grid container spacing={3} justifyContent="space-between" marginTop={5}>
+                                {/* Leaders of the company */}
+                                <Grid item xs={12} md={6}>
+                                    <Paper
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            height: 220,
+                                            borderRadius: 3
+                                        }}
+                                    >
+                                        <Chart />
+                                    </Paper>
+                                </Grid>
 
-                    <Grid container spacing={3} justifyContent="space-between" marginTop={5}>
-                        {/* Leaders of the company */}
-                        <Grid item xs={12} md={6}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    height: 220,
-                                    borderRadius: 3
-                                }}
-                            >
-                                <Chart />
-                            </Paper>
+                                <Grid item xs={12} md={6}>
+                                    <Paper
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            height: 220,
+                                            borderRadius: 3
+                                        }}
+                                    >
+                                        <ListOfLeaders />
+                                    </Paper>
+                                </Grid>
+                            </Grid>
                         </Grid>
 
-                        <Grid item xs={12} md={6}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    height: 220,
-                                    borderRadius: 3
-                                }}
-                            >
-                                <ListOfLeaders />
-                            </Paper>
+                        <Grid item xs={12} md={12} lg={12}>
+                            <Grid container spacing={3} justifyContent="space-between" marginTop={5}>
+                                {/* Other components */}
+                                {/* ... */}
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Grid>
-
-                <Grid item xs={12} md={12} lg={12}>
-
-                    <Grid container spacing={3} justifyContent="space-between" marginTop={5}>
-                        {/* Leaders of the company */}
-                        <Grid item xs={12} md={6}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    height: 220,
-                                    borderRadius: 3
-                                }}
-                            >
-                            </Paper>
-                        </Grid>
-
-                        <Grid item xs={12} md={6}>
-                            <Paper
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    height: 220,
-                                    borderRadius: 3
-                                }}
-                            >
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </Grid>
+                    </>
+                )}
             </Grid>
         </Grid>
     );
