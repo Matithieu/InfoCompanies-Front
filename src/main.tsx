f4e6953ad6b917e842f0072253ebbae25f26d0cb
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-01-30 21:21:38 +0100

    feat: QueryClientProvider for React Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Router>
-            <App />
-            <ToastContainer position='top-right' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <App />
+                <ToastContainer position='top-right' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+            </Router>
+        </QueryClientProvider>
     </React.StrictMode>,
 )
