0d51457c016283b5fb38abf7a01529da4247fd5a
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-02-28 18:18:28 +0100

    feat: add PlayWright

---
+++ tests/login.spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from "@playwright/test";
+
+const BASE_URL = "http://localhost:5173";
+
+test.beforeEach(async ({ page }) => {
+  // Go to the login page
+  await page.goto(`${BASE_URL}/login`);
+
+  await page.fill('input[id="email"]', "test@gmail.com");
+  await page.fill('input[id="password"]', "azertyuiop");
+
+  await page.click('button[aria-label="submit-login"]');
+
+  await page.waitForLoadState("networkidle");
+
+  const url = page.url();
+  console.log(url);
+
+  expect(url).toBe(`${BASE_URL}/dashboard`);
+});
+
+test("search a company and click on it", async ({ page }) => {
+  const searchTerm = "computerline";
+  const idOfCompany = 860868;
+
+  await page.click('input[id="search-company"]');
+  await page.fill('input[id="search-company"]', searchTerm);
+
+  await page.keyboard.press("Enter");
+
+  // wait for the table to be loaded
+  await page.waitForLoadState("networkidle");
+  await page.waitForTimeout(3000);
+
+  await page.click(`tr[id="company-${idOfCompany}"]`);
+});
