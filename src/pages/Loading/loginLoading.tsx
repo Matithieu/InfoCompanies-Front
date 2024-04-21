dd07f804253e515dc2eff609be9d45c0ccd35cdf
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-04-21 19:53:35 +0200

    attempt to fix user query

--- src/pages/Loading/loginLoading.tsx
+++ src/pages/Loading/loginLoading.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 
@@ -14,23 +15,29 @@ export default function FakeLoading() {
     queryKey: ["user query"],
     queryFn: () => fetchUser(),
     retry: 1,
-    refetchOnWindowFocus: false,
+    retryDelay: 2000,
+    refetchInterval: 2000,
+    refetchIntervalInBackground: true,
   })
 
-  if (data !== null && data !== undefined) {
-    console.log("Data fetched")
-    setAuthUser(data)
-    navigate("/dashboard")
-  } else {
-    console.log("No data")
-  }
+  useEffect(() => {
+    if (data !== null && data !== undefined) {
+      setAuthUser(data)
+      navigate("/dashboard")
+    } else {
+      console.error("No data")
+      // print a error page
+    }
+  }, [data, setAuthUser, navigate])
 
   if (isError) {
     return (
-      <div>
-        <h1>Failed to fetch user data</h1>
-        <p>{error.message}</p>
-      </div>
+      <>
+        <div>
+          <h1>Failed to fetch user data</h1>
+          <p>{error.message}</p>
+        </div>
+      </>
     )
   }
 
