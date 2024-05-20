6d8c61564268bca2ed83da45591d50489debfde5
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-05-20 18:40:25 +0200

    remove oidc provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import "./index.css"
 
 import { CssBaseline, CssVarsProvider } from "@mui/joy"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { OidcClientSettings } from "oidc-client-ts"
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { AuthProvider } from "react-oidc-context"
 import { BrowserRouter as Router } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 
@@ -26,17 +24,8 @@ const queryClient = new QueryClient({
   },
 })
 
-const oidcConfig: OidcClientSettings = {
-  authority: `${import.meta.env.VITE_KEYCLOAK_AUTHORITY}`,
-  client_id: `${import.meta.env.VITE_KEYCLOAK_CLIENT_ID}`,
-  response_type: "code",
-  redirect_uri: "http://localhost/loading",
-  post_logout_redirect_uri: `${import.meta.env.VITE_FRONTEND_URL}`,
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider {...oidcConfig}>
       <QueryClientProvider client={queryClient}>
         <Router>
           <CssVarsProvider theme={fontFamily}>
@@ -56,6 +45,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </CssVarsProvider>
         </Router>
       </QueryClientProvider>
-    </AuthProvider>
   </React.StrictMode>
 )
