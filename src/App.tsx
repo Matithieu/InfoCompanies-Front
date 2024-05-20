a67a417603d18f9e2118cac93fdfde49ff94eaab
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-05-20 18:40:14 +0200

    refactor: Optimize routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Suspense } from "react"
+import { lazy } from "@loadable/component"
+import { Suspense, useEffect } from "react"
 import { HelmetProvider } from "react-helmet-async"
 import { Route, Routes } from "react-router-dom"
-import { lazy } from "@loadable/component"
 
 import Error401 from "./pages/Error/401.tsx"
 import Error404 from "./pages/Error/404.tsx"
@@ -10,7 +10,6 @@ import Landing from "./pages/Landing/index.tsx"
 import Layout from "./pages/Layout/index.tsx"
 import Loading from "./pages/Loading/index.tsx"
 import Failure from "./pages/Purchasing/failure.tsx"
-import ViewInvoices from "./pages/Purchasing/invoice.tsx"
 import OrderConfirmation from "./pages/Purchasing/success.tsx"
 import Payment from "./pages/Stripe/index.tsx"
 import Subscription from "./pages/Subscription/index.tsx"
@@ -27,39 +26,44 @@ const Account = lazy(() => import("./pages/Account/index.tsx"))
 const Search = lazy(() => import("./pages/Search/index.tsx"))
 const Company = lazy(() => import("./pages/Search/Company/index.tsx"))
 const Leader = lazy(() => import("./pages/Leaders/index.tsx"))
-const FakeLoading = lazy(() => import("./pages/Loading/loginLoading.tsx"))
 
 function App() {
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      window.location.href = "/ui"
+    }
+  }, [])
+
   return (
     <HelmetProvider>
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route element={<Landing />} path="/" />
-          <Route element={<Test />} path="/test" />
-          <Route element={<FakeLoading />} path="/loading" />
-          <Route element="/error">
-            <Route element={<Error404 />} path="*/" />
-            <Route element={<Error401 />} path="/not-found" />
-          </Route>
-          z
-          <Route element={<ProtectedSimpleRoutes />}>
-            <Route element={<Subscription />} path="/subscription" />
-            <Route element={<Payment />} path="/stripe" />
-            <Route element={<Failure />} path="/failure" />
-            <Route element={<OrderConfirmation />} path="/completion" />
-          </Route>
-          <Route element={<ProtectedRoutes />}>
-            <Route element={<ViewInvoices />} path="/invoices" />
+          <Route path="/ui">
+          <Route element={<Landing />} path="" />
+            <Route element={<Test />} path="test" />
 
-            <Route element={<Layout />}>
-              <Route element={<Dashboard />} path="/dashboard" />
-              <Route element={<Favorites />} path="/favorites" />
-              <Route element={<Settings />} path="/settings" />
-              <Route element={<Account />} path="/account" />
-              <Route element={<Search />} path="/search/:searchTerm" />
-              <Route element={<Company />} path="/company/*" />
-              <Route element={<Leader />} path="/leaders/*" />
-              <Route element={<Error404 />} path="/*" />
+            <Route element="error">
+              <Route element={<Error404 />} path="*/" />
+              <Route element={<Error401 />} path="not-found" />
+            </Route>
+
+            <Route element={<ProtectedSimpleRoutes />}>
+              <Route element={<Subscription />} path="subscription" />
+              <Route element={<Payment />} path="stripe" />
+              <Route element={<Failure />} path="failure" />
+              <Route element={<OrderConfirmation />} path="completion" />
+            </Route>
+
+            <Route element={<ProtectedRoutes />}>
+              <Route element={<Layout />}>
+                <Route element={<Dashboard />} path="dashboard" />
+                <Route element={<Favorites />} path="favorites" />
+                <Route element={<Settings />} path="settings" />
+                <Route element={<Account />} path="account" />
+                <Route element={<Search />} path="search/:searchTerm" />
+                <Route element={<Company />} path="company/*" />
+                <Route element={<Leader />} path="leaders/*" />
+              </Route>
             </Route>
           </Route>
         </Routes>
