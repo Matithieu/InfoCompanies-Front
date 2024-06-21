629e879769a0b2b54634f8175fcd5a6402c2af41
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-06-21 17:26:21 +0200

    refactor: use readonly for consistency

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,24 +1,24 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_STRIPE_PUBLIC_KEY: string
-  VITE_STRIPE_SECRET_KEY: string
+  readonly VITE_STRIPE_PUBLIC_KEY: string
+  readonly VITE_STRIPE_SECRET_KEY: string
 
-  VITE_FRONTEND_URL: string
+  readonly VITE_FRONTEND_URL: string
 
-  VITE_PROXY_BASE_URL: string
-  VITE_PROXY_PORT: string
+  readonly VITE_PROXY_BASE_URL: string
+  readonly VITE_PROXY_PORT: string
 
-  VITE_OAUTH_BASE_URL: string
-  VITE_OAUTH_SIGNIN_URL: string
-  VITE_OAUTH_SIGNOUT_URL: string
-  VITE_OAUTH_SIGNIN_REDIRECT_URL: string
-  VITE_OAUTH_SIGNOUT_REDIRECT_URL: string
+  readonly VITE_OAUTH_BASE_URL: string
+  readonly VITE_OAUTH_SIGNIN_URL: string
+  readonly VITE_OAUTH_SIGNOUT_URL: string
+  readonly VITE_OAUTH_SIGNIN_REDIRECT_URL: string
+  readonly VITE_OAUTH_SIGNOUT_REDIRECT_URL: string
 
-  VITE_API_BASE_URL: string
-  VITE_API_PREFIX: string
-  VITE_API_BASE_V1_URL: string
-  VITE_API_COMPANY_ENDPOINT: string
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_API_PREFIX: string
+  readonly VITE_API_BASE_V1_URL: string
+  readonly VITE_API_COMPANY_ENDPOINT: string
 }
 
 interface ImportMeta {
