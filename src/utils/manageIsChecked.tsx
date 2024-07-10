f2b2b6409405df808894402f128b1dc9b8fb88b4
Mathieu Yahia-Amar <mathieu.yahiaamar@pelico.io>
2024-07-10 22:11:42 +0200

    refactor: use Set instead of array

--- src/utils/manageIsChecked.tsx
+++ src/utils/manageIsChecked.tsx
@@ -4,31 +4,23 @@ import { companiesSeenStorage } from './localStorage/companiesSeenStorage'
 export const manageIsChecked = (
   companyId: number,
   currentStatus: CheckStatus,
-) => {
+): void => {
   const { companiesToDo, companiesDone } = companiesSeenStorage()
-  const checkedToDo = companiesToDo.getCompaniesTodo()
-  const checkedDone = companiesDone.getCompaniesDone()
+  const checkedToDo = new Set(companiesToDo.getCompaniesTodo())
+  const checkedDone = new Set(companiesDone.getCompaniesDone())
 
-  const removeFromList = (list: number[], id: number) => {
-    const index = list.indexOf(id)
+  // Remove the companyId from both sets
+  checkedToDo.delete(companyId)
+  checkedDone.delete(companyId)
 
-    if (index !== -1) {
-      list.splice(index, 1)
-    }
+  // Add the companyId to the appropriate set based on currentStatus
+  if (currentStatus === CheckStatus.DONE) {
+    checkedDone.add(companyId)
+  } else if (currentStatus === CheckStatus.TO_DO) {
+    checkedToDo.add(companyId)
   }
 
-  removeFromList(checkedDone, companyId)
-  removeFromList(checkedToDo, companyId)
-
-  switch (currentStatus) {
-    case CheckStatus.DONE:
-      checkedDone.push(companyId)
-      break
-    case CheckStatus.TO_DO:
-      checkedToDo.push(companyId)
-      break
-  }
-
-  companiesToDo.updateCompaniesTodo(checkedToDo)
-  companiesDone.updateCompaniesDone(checkedDone)
+  // Update the storage with the new sets
+  companiesToDo.updateCompaniesTodo(Array.from(checkedToDo))
+  companiesDone.updateCompaniesDone(Array.from(checkedDone))
 }
