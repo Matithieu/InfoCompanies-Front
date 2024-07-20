4f34aefc9bad99920939970b21dc80b73478abb7
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-07-20 22:07:16 +0200

    feat: improve text alignment

--- src/components/common/Texts/index.tsx
+++ src/components/common/Texts/index.tsx
@@ -3,7 +3,14 @@ import { FC } from 'react'
 
 export const PleaseSelectACompanyText: FC = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Typography variant="h6">Veuillez sélectionner une entreprise</Typography>
     </div>
   )
