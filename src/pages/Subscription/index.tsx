add498ce3c5b1de72f54d0d2bdb09d5370d639b3
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-08-12 23:26:30 +0200

    refactor: Simplify Subscription page structure

--- src/pages/Subscription/index.tsx
+++ src/pages/Subscription/index.tsx
@@ -1,31 +1,25 @@
-import ReusableContainer from '../../components/common/ReusableContainer'
 import Footer from '../../components/pages/footer'
-import PageStyleWrapper from '../../components/pages/PageStyleWrapper/pageStyleWrapper'
 import Subscriptions from '../../components/parts/Subscription'
 import HeaderLanding from '../Landing/components/header'
 
 const SubscriptionPage: React.FC = () => {
   return (
-    <PageStyleWrapper>
-      <div>
-        <HeaderLanding />
-        <ReusableContainer style={{}}>
-          <div className="landing-container">
-            <Subscriptions />
-          </div>
-        </ReusableContainer>
-        <div
-          style={{
-            position: 'fixed',
-            bottom: 0,
-            left: 0,
-            right: 0,
-          }}
-        >
-          <Footer />
-        </div>
+    <div>
+      <HeaderLanding />
+      <div className="landing-container">
+        <Subscriptions />
       </div>
-    </PageStyleWrapper>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        <Footer />
+      </div>
+    </div>
   )
 }
 
