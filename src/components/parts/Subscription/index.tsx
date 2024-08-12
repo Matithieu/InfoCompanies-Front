7cfe211e6036dabce60eb4379c5c2d08dfd219e6
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-08-12 23:15:56 +0200

    improve UI

--- src/components/parts/Subscription/index.tsx
+++ src/components/parts/Subscription/index.tsx
@@ -1,10 +1,10 @@
 import { Box } from '@mui/material'
-import React, { useState } from 'react'
+import { FC, useState } from 'react'
 
 import { products } from '../../../data/Stripe/subscription'
 import SubscriptionCard from './components/SubscriptionCard'
 
-const Subscriptions: React.FC = () => {
+const Subscriptions: FC = () => {
   const [buttonClicked, setButtonClicked] = useState(false)
 
   const subscriptions = products.sort((a, b) => a.price - b.price)
@@ -17,7 +17,7 @@ const Subscriptions: React.FC = () => {
     <Box
       sx={{
         display: 'flex',
-        flexDirection: { xs: 'column', md: 'column', lg: 'row' },
+        flexDirection: { xs: 'column', sm: 'column', md: 'column', lg: 'row' },
         justifyContent: 'center',
         padding: '20px',
         gap: '30px',
