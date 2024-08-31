7bcb24ea8e45a46e0fd2ba501e1ab6e9881c6e92
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-08-31 16:48:33 +0200

    refactor: lower minWidth + remplace column

--- src/data/types/columns.ts
+++ src/data/types/columns.ts
@@ -8,16 +8,16 @@ export const columnsTableCompany: Column[] = [
   { id: 'checked', label: '', minWidth: 100, align: 'center' },
   { id: 'companyName', label: 'Denomination', minWidth: 170 },
   { id: 'phoneNumber', label: 'Téléphone', minWidth: 170 },
-  { id: 'email', label: 'Email', minWidth: 170 },
+  { id: 'email', label: 'Email', minWidth: 140 },
   { id: 'website', label: 'Site Web', minWidth: 170 },
 
   { id: 'socialMedia', label: 'Réseaux Sociaux', minWidth: 170 },
 
+  { id: 'numberOfEmployee', label: 'Salariés', minWidth: 100 },
+  { id: 'city', label: 'Ville', minWidth: 170 },
+  { id: 'dateRegistration', label: 'Date Immatriculation', minWidth: 170 },
   { id: 'industrySector', label: "Secteur d'Activité", minWidth: 170 },
   { id: 'legalForm', label: 'Forme Juridique', minWidth: 170 },
   { id: 'address', label: 'Adresse', minWidth: 170 },
-  { id: 'postalCode', label: 'Code Postal', minWidth: 170 },
-  { id: 'city', label: 'Ville', minWidth: 170 },
   { id: 'region', label: 'Région', minWidth: 170 },
-  { id: 'dateRegistration', label: 'Date Immatriculation', minWidth: 170 },
 ]
