f1924b650a3b8894ee060ac8bc0a663d2f3c71d1
Matithieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-11-18 21:14:40 +0100

    testing component

--- src/pages/Test/TestPage.tsx
+++ src/pages/Test/TestPage.tsx
@@ -1,10 +1,36 @@
 import { Button } from '@/components/ui/button copy'
+import AddIcon from '@mui/icons-material/Add'
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+import { Typography } from '@mui/material'
 import { FC } from 'react'
 
 const Test: FC = () => {
   return (
     <>
-      <Button>Test</Button>
+      <div>
+        <Button>
+          <AddIcon style={{ fontSize: '1.2rem' }} />
+        </Button>
+      </div>
+
+      <div style={{ padding: 10 }} />
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <Button style={{ borderRadius: '50%' }}>
+          <KeyboardArrowLeftIcon />
+        </Button>
+        <Typography variant="body2"> 1 / 10 </Typography>
+        <Button style={{ borderRadius: '50%' }}>
+          <KeyboardArrowRightIcon />
+        </Button>
+      </div>
     </>
   )
 }
